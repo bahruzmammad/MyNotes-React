87ffbb94bc10fe67c309e144978e850d087ff69b
Bahruz Mammadov <bahruzmammadly@gmail.com>
2026-09-06 19:29:17 +0400

    Upgraded App.Js and added rest app for backend connecting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-// import App from './App.jsx'
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App />*/}
-
-    <div>
-      <h1>My Notes</h1>
-    </div>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
